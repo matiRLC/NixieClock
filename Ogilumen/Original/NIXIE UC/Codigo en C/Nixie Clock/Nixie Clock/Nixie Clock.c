9006c7c07b17cd128e08361c41ff2dd168352b08
Matias Quintana <matias.quintana.r@gmail.com>
2017-07-30 13:39:08 -0400

    First upload of existing files

---
+++ Ogilumen/Original/NIXIE UC/Codigo en C/Nixie Clock/Nixie Clock/Nixie Clock.c
@@ -0,0 +1,19 @@
+/*
+ * Nixie_Clock.c
+ *
+ * Created: 9/21/2012 9:45:31 AM
+ *  Author: Matias
+ */ 
+
+#include <avr/io.h>
+
+
+
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
